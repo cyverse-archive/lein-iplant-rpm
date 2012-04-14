8279f0a92bc1705955beba98724e42df55e61544
Dennis Roberts <dennis@iplantcollaborative.org>
2012-04-13 17:47:15 -0700

    CORE-3104: added code to create the source tarball that will be used to generate the RPM

--- src/leiningen/iplant_rpm.clj
+++ src/leiningen/iplant_rpm.clj
@@ -1,10 +1,19 @@
 (ns leiningen.iplant-rpm
-  (:use [clojure.java.io :only (file copy)]
-        [clojure.string :only (split)]
-        [fleet]))
+  (:use [clojure.java.io :only [file copy reader]]
+        [clojure.string :only [join split]]
+        [fleet]
+        [leiningen.compile :only [sh]]))
 
+;; The paths to the template files, relative to the classpath.
 (def ^{:private true} spec-path "rpm/spec.fleet")
 (def ^{:private true} init-path "rpm/init.fleet")
+
+;; The path to various RPM directories.
+(def ^{:private true} rpm-base-dir (file "/usr/src/redhat"))
+(def ^{:private true} rpm-spec-dir (file rpm-base-dir "SPECS"))
+(def ^{:private true} rpm-soruce-dir (file rpm-base-dir "SOURCES"))
+(def ^{:private true} rpm-build-dir (file rpm-base-dir "BUILD"))
+(def ^{:private true} rpm-dir (file rpm-base-dir "RPMS"))
 
 (defn- slurp-resource
   "Slurps the contents of a resource that can be found relative to a location
@@ -35,7 +44,7 @@
   "Prints a warning message to standard error output."
   [& ms]
   (binding [*out* *err*]
-    (println (apply str ms))
+    (println (join " " ms))
     (flush)))
 
 (defn- gen-file
@@ -71,9 +80,30 @@
           :else (throw (Exception. "unrecognized file type")))))
 
 (defn- rec-copy
-  "Performs a recursive copy of one or more files."
+  "Performs a recursive copy of one or more files.  Note that recursion does
+   consume stack space.  This shouldn't be a problem, however, because a
+   directory structure that is deep enough to cause a stack overflow will
+   probably create a path that is too long for the OS to support."
   [dir fs]
   (dorun (map #(copy-file-or-dir dir %) fs)))
+
+(defn- rec-delete
+  "Recursively deletes all files in a directory structure rooted at the given
+   directory.  Note that this recursion does consume stack space.  This
+   shouldn't be a problem, however, because a directory structure that is deep
+   enough to cause a stack overflow will probably create a path that is too
+   long for the OS to support."
+  [f]
+  (when (.isDirectory f)
+    (dorun (map #(rec-delete %) (.listFiles f))))
+  (.delete f))
+
+(defn- build-spec-file
+  "Builds the RPM specification file."
+  [settings]
+  (let [spec-name (str (:name settings) ".spec")]
+    (gen-file settings spec-name spec-path)
+    (copy (file spec-name) (file rpm-spec-dir spec-name))))
 
 (defn- make-build-dir
   "Creates the build directory, which will be used to generate the source
@@ -84,16 +114,33 @@
     (rec-copy build-dir (map #(file %) [init-name "project.clj" "src"]))
     (rec-copy (.getParentFile (file build-dir config-dir)) [config-dir])))
 
+(defn- exec
+  "Executes a command, throwing an exception if the command fails."
+  [& args]
+  (let [status (apply sh args)]
+    (when (not= status 0)
+      (let [cmd (join " " args)]
+        (throw (Exception. (str cmd " failed with status " status)))))))
+
+(defn- build-source-tarball
+  "Builds the source tarball that will be used by rpmbuild to generate the
+   RPM and returns the base name of the generated tarball, which is needed
+   for cleanup work."
+  [settings]
+  (let [build-dir (file (str (:name settings) "-" (:version settings)))
+        init-name (:name settings)]
+    (gen-file settings init-name init-path)
+    (make-build-dir build-dir settings init-name)
+    (exec "tar" "czvf" (str build-dir ".tar.gz") (.getPath build-dir))
+    (rec-delete build-dir)
+    build-dir))
+
 (defn- build-rpm
   "Builds the RPM."
   [prj]
-  (let [settings (project-to-settings prj)
-        init-name (:name settings)
-        spec-name (str init-name ".spec")
-        build-dir (file (str (:name settings) "-" (:version settings)))]
-    (gen-file settings init-name init-path)
-    (gen-file settings spec-name spec-path)
-    (make-build-dir build-dir settings init-name)))
+  (let [settings (project-to-settings prj)]
+    ;;(build-spec-file settings)
+    (build-source-tarball settings)))
 
 (defn iplant-rpm
   "Generates the type of RPM that is used by the iPlant Collaborative to
