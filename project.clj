cd9658f96acfa0aee127fa006d97d016cac1ad2e
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-20 15:11:56 -0700

    clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.iplantc/lein-iplant-rpm "1.4.1-SNAPSHOT"
   :eval-in-leiningen true
   :description "Leiningen Plugin for generating RPMs for Clojure projects."
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [fleet "0.10.1"]]
   :repositories {"iplantCollaborative"
                  "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
