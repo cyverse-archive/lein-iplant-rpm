07b1c05370bd34b45dd82075f6e8fb7480b1b36e
John Wregglesworth <wregglej@iplantcollaborative.org>
2013-05-21 08:05:17 -0700

    Added the ability (hopefully) to add :exe-files to a service.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.iplantc/lein-iplant-rpm "1.4.1-SNAPSHOT"
+(defproject org.iplantc/lein-iplant-rpm "1.4.2-SNAPSHOT"
   :eval-in-leiningen true
   :description "Leiningen Plugin for generating RPMs for Clojure projects."
   :dependencies [[org.clojure/clojure "1.5.1"]
