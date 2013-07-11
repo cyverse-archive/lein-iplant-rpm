8f9d8ab8df7fdfded5137f3decc2dd733eb2bad0
Dennis Roberts <dennis@iplantcollaborative.org>
2013-07-11 10:33:44 -0700

    CORE-4553: switched back to fleet version 0.9.5

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :eval-in-leiningen true
   :description "Leiningen Plugin for generating RPMs for Clojure projects."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [fleet "0.10.1"]]
+                 [fleet "0.9.5"]]
   :repositories {"iplantCollaborative"
                  "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
