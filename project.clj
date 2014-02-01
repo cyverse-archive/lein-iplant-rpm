fe75b889a651a70b8fcfbadbaab5e1b1c1abb070
Dennis Roberts <dennis@iplantcollaborative.org>
2014-01-31 17:44:40 -0700

    added some elements that are required by sonatype to pom.xml

--- project.clj
+++ project.clj
@@ -1,7 +1,17 @@
 (defproject org.iplantc/lein-iplant-rpm "1.4.3-SNAPSHOT"
   :eval-in-leiningen true
   :description "Leiningen Plugin for generating RPMs for Clojure projects."
+  :url "http://www.iplantcollaborative.org"
+  :license {:name "BSD"
+            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :scm {:connection "scm:git:git@github.com:iPlantCollaborativeOpenSource/lein-iplant-rpm.git"
+        :developerConnection "scm:git:git@github.com:iPlantCollaborativeOpenSource/lein-iplant-rpm.git"
+        :url "git@github.com:iPlantCollaborativeOpenSource/lein-iplant-rpm.git"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [fleet "0.9.5"]]
   :repositories {"iplantCollaborative"
-                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"})
+                 "http://projects.iplantcollaborative.org/archiva/repository/internal/"}
+  :repositories [["sonatype-nexus-snapshots"
+                  {:url "https://oss.sonatype.org/content/repositories/snapshots"}]
+                 ["sonatype-nexus-staging"
+                  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"}]])
